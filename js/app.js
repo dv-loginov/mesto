e26a5032cd9049a46e1b61d2d5606b1536e3314d
dv-loginov <dilog@rambler.ru>
2023-01-19 18:13:42 +0300

    feat: верстка десктопной версии и прграммирование логики попапа

--- js/app.js
+++ js/app.js
@@ -1,1 +1,29 @@
-console.log('app run');
+const formElement = document.querySelector('.form');
+const nameInput = formElement.querySelector('.form__input[name="name"]');
+const jobInput = formElement.querySelector('.form__input[name="job"]');
+
+const title = document.querySelector('.profile__title');
+const subtitle = document.querySelector('.profile__subtitle');
+
+const editBtn = document.querySelector('.profile__edit-btn');
+const popup = document.querySelector('.popup');
+const closePopup = document.querySelector('.popup__close');
+
+editBtn.onclick = () => {
+    popup.style.display = 'flex';
+    nameInput.value = title.textContent;
+    jobInput.value = subtitle.textContent;
+}
+
+closePopup.onclick = () => {
+    popup.style.display = 'none';
+}
+
+function handleFormSubmit (evt) {
+    evt.preventDefault(); 
+    title.textContent = nameInput.value;
+    subtitle.textContent = jobInput.value;
+    popup.style.display = 'none';
+}
+
+formElement.addEventListener('submit', handleFormSubmit);
